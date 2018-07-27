5320de0ee47f7cbc6bf1a049e8d582b3c318bd2f
Dominik Kohler <dominik.kohler@lextira.ch>
2018-07-27 23:29:56 +0200

    changed order of fields

--- src/database/migrations/2018_07_27_211226_create_accounts_table.php
+++ src/database/migrations/2018_07_27_211226_create_accounts_table.php
@@ -17,10 +17,10 @@ class CreateAccountsTable extends Migration
             $table->increments('id');
             $table->foreign('domain_id')->references('id')->on('domains');
             $table->string('name');
+            $table->string('relative_dir');
             $table->string('login')->unique();
             $table->string('password');
             $table->boolean('status');
-            $table->string('relative_dir');
             $table->string('absolute_dir');
             $table->timestamps();
         });
