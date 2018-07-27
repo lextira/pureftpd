22f4a498bb95e34d741935ffd1f4208c55afa8de
Dominik Kohler <dominik.kohler@lextira.ch>
2018-07-27 23:25:55 +0200

    created migrations

---
+++ src/database/migrations/2018_07_27_211219_create_keys_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('domain_id')->references('id')->on('domains');
+            $table->string('key')->unique();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keys');
+    }
+}
---
+++ src/database/migrations/2018_07_27_211226_create_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('domain_id')->references('id')->on('domains');
+            $table->string('name');
+            $table->string('login')->unique();
+            $table->string('password');
+            $table->boolean('status');
+            $table->string('relative_dir');
+            $table->string('absolute_dir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
